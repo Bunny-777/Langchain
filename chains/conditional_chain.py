8b31b8cf92c570d57a5845ab66a8ed9a9ad190f3
Bunny <bunny202410@gmail.com>
2026-06-19 01:04:43 +0530

    conditional_chain fixed

--- chains/conditional_chain.py
+++ chains/conditional_chain.py
@@ -2,10 +2,11 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableParallel
+from langchain_core.runnables import RunnableParallel,RunnableBranch, RunnableLambda
 from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel,Field
 from typing import Literal
+
 
 load_dotenv()
 
@@ -20,12 +21,32 @@ llm=HuggingFaceEndpoint(
     task="text-generation"
 )
 model=ChatHuggingFace(llm=llm)
-prompt1=PromptTemplate(
-    template="From the following {feedback} classify the sentiment into positive or negative \n {format_instruction}",
-    input_variables=["feedback"],
-    partial_variables={"format_instruction":parser2.get_format_instructions()}
+
+prompt1 = PromptTemplate(
+    template='Classify the sentiment of the following feedback text into postive or negative \n {feedback} \n {format_instruction}',
+    input_variables=['feedback'],
+    partial_variables={'format_instruction':parser2.get_format_instructions()}
 )
 
-chain=prompt1 | model | parser2
-result=chain.invoke({"feedback":"This is so awesome product"})
-print(result.sentiment)
+prompt2 = PromptTemplate(
+    template='Write an appropriate response to this positive feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+prompt3 = PromptTemplate(
+    template='Write an appropriate response to this negative feedback \n {feedback}',
+    input_variables=['feedback']
+)
+
+classifier_chain = prompt1 | model | parser2
+branch_chain = RunnableBranch(
+    (lambda x:x.sentiment == 'positive', prompt2 | model | parser),
+    (lambda x:x.sentiment == 'negative', prompt3 | model | parser),
+    RunnableLambda(lambda x: "could not find sentiment")
+)
+
+chain = classifier_chain | branch_chain
+
+print(chain.invoke({'feedback': 'This is a beautiful phone'}))
+
+chain.get_graph().print_ascii()
