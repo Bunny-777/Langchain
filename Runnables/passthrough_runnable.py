4eb22378fa2e6c2b137b757644b21eab63fccfb1
Bunny <bunny202410@gmail.com>
2026-06-21 22:03:30 +0530

    passthrough_runnable

---
+++ Runnables/passthrough_runnable.py
@@ -0,0 +1,29 @@
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableSequence,RunnableParallel,RunnablePassthrough
+from dotenv import load_dotenv
+load_dotenv()
+
+llm=ChatGroq(model="llama-3.3-70b-versatile")
+parser=StrOutputParser()
+
+prompt1=PromptTemplate(
+    template="Generate a short poem on the {topic}",
+    input_variables=['topic']
+)
+
+prompt2=PromptTemplate(
+    template="Summarise the poem from the given {text}",
+    input_variables=['text']
+)
+
+poem_chain=RunnableSequence(prompt1,llm,parser)
+
+parallel_chain=RunnableParallel({
+    "poem":RunnablePassthrough(),
+    "summary":RunnableSequence(prompt2,llm,parser)
+})
+
+final_chain=RunnableSequence(poem_chain,parallel_chain)
+print(final_chain.invoke({"topic":"girlfriend"}))
