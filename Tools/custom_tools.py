bf72e8e584b18303b530e400bdc6640629dbc409
Bunny <bunny202410@gmail.com>
2026-06-30 11:44:49 +0530

    using @tool

---
+++ Tools/custom_tools.py
@@ -0,0 +1,19 @@
+from langchain_core.tools import tool
+
+def multiply(a,b):
+    return a*b
+
+def multiply(a:int ,b:int) -> int:
+        """Multiply the given two numbers"""
+        return a*b
+
+@tool
+def multiply(a:int, b:int) ->int:
+      """Mutltiply the given two numbers"""
+      return a*b
+
+result=multiply.invoke({"a":4,"b":5})
+print(result)
+
+
+
